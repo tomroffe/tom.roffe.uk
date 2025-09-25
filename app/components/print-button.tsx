4d9c3c29ffc598846a3a2f591661f7c86bc8037c
Tom Roffe <tom.roffe@gmail.com>
2025-09-25 09:45:11 +0100

    feat: Adding print button component

--- app/components/print-button.tsx
+++ app/components/print-button.tsx
@@ -13,10 +13,10 @@ export default function PrintButton() {
     <Button
       id="print-button"
       color="dark"
-      className="print:hidden rounded-lg p-2.5 mx-2 text-sm text-gray-500 bg-white hover:bg-gray-100 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-700"
+      className="print:hidden rounded-lg p-2.5 mx-2 text-sm text-gray-500 bg-white hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-700"
       onClick={() => window.print()}
     >
-      <PrinterIcon className='w-4 h-4' />
+      <PrinterIcon className='w-4.5 h-4.5 stroke-2' />
     </Button>
   );
 }
