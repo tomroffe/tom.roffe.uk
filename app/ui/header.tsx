590cfc00fdbb6a3a6e86a60b34b53a23c23486f1
Tom Roffe <tom.roffe@gmail.com>
2025-09-25 09:46:54 +0100

    fix: various nav and link updates

--- app/ui/header.tsx
+++ app/ui/header.tsx
@@ -8,14 +8,19 @@ import PrintButton from '@/app/components/print-button';
 export default function Header() {
   return (
     <header className="text-gray-600 dark:text-gray-50 bg-white dark:bg-gray-800 body-font print:hidden">
-      <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-        <Link href="/" className='flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0' >
+      <div className="container mx-auto flex flex-wrap p-3 md:p-5 flex-row items-center justify-between">
+        <Link href="/" className='flex-none flex ustify-self-start title-font font-medium items-center text-gray-900 mr-1' >
           <TARLogo className="w-10 h-10 text-white p-2 bg-gray-300 rounded-full" />
           <span className="ml-3 text-lg font-bold dark:text-gray-50">Tom Roffe</span>
         </Link>
         <NavLinks />
-        <PrintButton />
-        <DarkThemeToggle />
+
+
+
+
+        <nav className="flex justify-end  md:border-gray-400 text-base">
+          <DarkThemeToggle />
+        </nav>
         {/* <Link */}
         {/*   href="/login" */}
         {/*   className="flex items-center rounded-md  px-4 py-2 ml-2 text-sm font-medium transition-colors  md:text-base " */}
--- app/ui/nav-links.tsx
+++ app/ui/nav-links.tsx
@@ -18,14 +18,14 @@ const links = [
 
 export default function NavLinks() {
   return (
-    <nav className="inline-flex justify-center md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-400 items-center text-base">
+    <nav className="inline-flex justify-center md:mt-0 md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-400 items-center text-base">
       {links.map((link) => {
         const LinkIcon = link.icon;
         return (
           <Link
             key={link.name}
             href={link.href}
-            className="inline-flex mr-5 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100"
+            className="inline-flex mx-1 md:mx-2 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100"
           >
             <LinkIcon className="w-6 mr-1" />
             <p className="block">{link.name}</p>
